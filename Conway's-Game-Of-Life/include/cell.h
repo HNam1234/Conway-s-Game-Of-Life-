1ac7ba2727196654c5cb3a31497cb4a48f4e32c3
unknown <hoainam190500@gmail.com>
2024-09-09 14:40:21 +0700

    .\Conway's-Game-Of-Life\

---
+++ Conway's-Game-Of-Life/include/cell.h
@@ -0,0 +1,34 @@
+#ifndef CELL_H
+#define CELL_H
+
+#include<iostream>
+#include"life.h"
+
+class cell{
+    //Cell status
+    bool alive;
+
+    public:
+        //Constructor
+        //Cells are empty by default
+        cell() : alive(false)   {}
+        //Draw the cell on the grid
+
+        void draw(int col,int row) const;
+
+        //Bring cell to life
+
+        void create()
+        {
+            alive = true;
+        }
+        //End cell's life
+        void erase()
+        {
+            alive = false;
+        }
+
+        bool is_alive() const {return alive;}
+};
+
+#endif //CELL_H
---
+++ Conway's-Game-Of-Life/include/life.h
@@ -0,0 +1,16 @@
+#ifndef LIFE_H
+#define LIFE_H
+
+const char live_cell{'X'};
+const char dead_cell{' '};
+
+const int rowmax = 23;
+const int colmax = 79;
+
+//conways parameters
+const int min_neighbours = 2;
+const int max_neighbours = 3;
+const int min_parents = 2;
+const int max_parents = 3;
+
+#endif //LIFE_H
